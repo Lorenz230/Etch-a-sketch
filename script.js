1c6ebf49fb58211b9d9d96430c66ec6d96717b2c
Lorenz230 <lorenzcamento0@gmail.com>
2023-07-27 00:11:47 +0100

    Allows users to draw on the grid
    etch-a-sketch has a basic function now.

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ const grid = document.querySelector('.grid')
 for(let i = 0; i < 256; i++){
     let newDiv = document.createElement('div');
     newDiv.style.backgroundColor = "white";
-    newDiv.style.border = '1px solid #e6e3e3';
+    // newDiv.style.border = '1px solid #e6e3e3';
     newDiv.style.height = "32px";
     newDiv.style.width = "32px";
     grid.append(newDiv);
 }
 
+const boxes = document.querySelectorAll(".grid div");
 
+boxes.forEach((box) => {
+    box.addEventListener('mousedown', change);
+    box.addEventListener('mouseover', change);
+
+});
+
+let mouseDown = false;
+grid.onmousedown = () => (mouseDown = true);
+grid.onmouseup = () => (mouseDown = false);
+
+function change(event){
+    console.log(mouseDown);
+    if(event.type === 'mouseover' && mouseDown){
+        event.target.style.backgroundColor = "black";
+    }
+}
 
